78ed61945382ed3784ca7af5dc17ecc56b372a8b
mateoaguirrecancela <mateoaguirrecancela@gmail.com>
2023-09-27 12:48:30 +0200

    titulo y nombres

--- p1.c
+++ p1.c
@@ -1,3 +1,8 @@
+//Practica 1 Algoritmos
+//Autor 1: Mateo Aguirre Cancela    |   mateo.aguirre
+//Autor 2: Martin del Rio Alvarez   |   martin.delrio.alvarez
+//Grupo: 2.2
+
 #include <stdio.h>
 
 // Algoritmo 1
