e048dbfd84e47d5cf7323f43a9d25e51dd419a0b
Usuario <delrioalvarezmartin@gmail.com>
2023-10-25 13:43:57 +0200

    inicio p3

--- P3/p3.c
+++ P3/p3.c
@@ -3,3 +3,17 @@
 // Autor 2: Martin del Rio Alvarez   |   martin.delrio.alvarez
 // Grupo: 2.2
 
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <math.h>
+#include <stdbool.h>
+
+double microsegundos(){ /*obtiene la hora del sistema en microsegundos*/
+    struct timeval t;
+    if (gettimeofday(&t, NULL) < 0){
+        return 0.0;
+    }
+    return (t.tv_usec + t.tv_sec * 1000000.0);
+}
