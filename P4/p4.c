e519c942a478b8e6733bd62d2a8a81e5aef54778
mateoaguirrecancela <mateoaguirrecancela@gmail.com>
2023-11-15 14:24:09 +0100

    test1

--- P4/p4.c
+++ P4/p4.c
@@ -14,25 +14,6 @@
 
 typedef int **matriz;
 
-void dijkstra(matriz grafo, matriz distancias, int tam)
-{
-    int n, i, j, min, v = 0;
-    int *noVisitados = malloc(tam * sizeof(int));
-    for (n = 0; n < tam; n++)
-    {
-        for (i = 0; i < tam; i++)
-        {
-            noVisitados[i] = 1;
-            distancias[n][i] = grafo[n][i];
-        }
-        noVisitados[n] = 0;
-        /*
-        ...
-        */
-    }
-    free(noVisitados);
-}
-
 matriz crearMatriz(int n)
 {
     int i;
@@ -67,6 +48,84 @@ void liberarMatriz(matriz m, int n)
     free(m);
 }
 
+void imprimirMatriz(matriz m, int n){
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%5d", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void introducirValores(matriz m, int **p, int n){
+    int i, j;
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            m[i][j]=p[i][j];
+        }
+    }
+}
+
+void dijkstra(matriz grafo, matriz distancias, int tam)
+{
+    int n, i, j, min, count, v = 0;
+    int *noVisitados = malloc(tam * sizeof(int));
+    for (n = 0; n < tam; n++)
+    {
+        for (i = 0; i < tam; i++)
+        {
+            noVisitados[i] = 1;
+            distancias[n][i] = grafo[n][i];
+        }
+        noVisitados[n] = 0;
+        /*
+        ...
+        */
+        for (count = 0; count < tam - 2; count++) {
+            min = TAM_MAX;
+
+            for (i = 0; i < tam; i++) {
+                if (noVisitados[i] && distancias[n][i] < min) {
+                    min = distancias[n][i];
+                    v = i;
+                }
+            }
+
+            noVisitados[v] = 0;
+
+            for (j = 0; j < tam; j++) {
+                if (noVisitados[j] && distancias[n][j] > distancias[n][v] + grafo[v][j]) {
+                    distancias[n][j] = distancias[n][v] + grafo[v][j];
+                }
+            }
+        }
+    }
+    free(noVisitados);
+}
+
+void test(){
+    int n=5;
+    int p[][5]={{0,1,8,4,7},{1,0,2,6,5},{8,2,0,9,5},{4,6,9,0,3},{7,5,5,3,0}};
+    matriz m, d;
+    m=crearMatriz(n);
+    introducirValores(m, p, n);
+    d=crearMatriz(n);
+
+    imprimirMatriz(m, n);
+    printf("\n");
+    dijkstra(m, d, n);
+    imprimirMatriz(m, n);
+    printf("\n");
+    imprimirMatriz(d, n);
+    printf("\n");
+
+    liberarMatriz(m, n);
+}
+
+
 void inicializar_semilla()
 {
     srand(time(NULL));
@@ -80,4 +139,29 @@ void aleatorio(int v[], int n)
     {
         v[i] = (rand() % m) - n;
     } /* se generan números pseudoaleatorio entre -n y +n */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+    inicializar_semilla();
+
+    test();
+
+    return 0;
 }
